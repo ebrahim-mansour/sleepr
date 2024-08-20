9e6473dc5843a58be816fe4e4f0bce77c4b0cc84
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2024-08-20 16:24:42 +0300

    add the abstract repository

---
+++ libs/common/src/database/abstract.schema.ts
@@ -0,0 +1,10 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { SchemaTypes, Types } from 'mongoose';
+
+@Schema()
+export class AbstractDocument {
+  @Prop({ type: SchemaTypes.ObjectId })
+  _id: Types.ObjectId;
+}
+
+// TODO: replace the class with an interface and check whether everything is working properly or not.
