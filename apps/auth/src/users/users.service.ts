24cfa6805c039f1ff98a809a6f1ccc26239d8634
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2024-09-02 18:22:21 +0300

    implement login API, jwt and local strategy

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
+import { GetUserDto } from './dto/get-user.dto';
+import { UserDocument } from './models/user.schema';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+    } catch (error) {
+      return;
+    }
+    throw new UnprocessableEntityException('Email already exists');
+  }
+
   async create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    await this.validateCreateUserDto(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      throw new UnauthorizedException('Credentials are not valid.');
+    return user;
+  }
+
+  async getUser(getUserDto: GetUserDto): Promise<UserDocument> {
+    return this.usersRepository.findOne(getUserDto);
   }
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,6 +1,8 @@
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import {
   ReservationDocument,
   ReservationSchema,
@@ -16,6 +18,13 @@ import { ReservationsService } from './reservations.service';
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
