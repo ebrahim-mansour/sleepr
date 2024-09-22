fb2c818de2dd1d1f2a8a5c004cf4f4d61a64e0e8
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2024-09-22 22:45:43 +0300

    add ingress and health module

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,6 +1,7 @@
 import {
   AUTH_SERVICE,
   DatabaseModule,
+  HealthModule,
   LoggerModule,
   PAYMENTS_SERVICE,
 } from '@app/common';
@@ -62,6 +63,7 @@ import { ReservationsService } from './reservations.service';
         inject: [ConfigService],
       },
     ]),
+    HealthModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
